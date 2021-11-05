b8b6f08778348dedbf3a4fc791aa8a2ae40fdaa4
unknown <you@example.com>
2021-11-05 23:57:29 +0300

    Odevler eklendi

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PozitifNegatifSayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı Giriniz:");
+            int i = int.Parse(Console.ReadLine());
+            if (i < 0)
+            {
+                Console.WriteLine("Sayı Negatif");
+            }
+            else if (i > 0)
+            {
+                Console.WriteLine("Sayı Pozitif");
+            }
+            else
+            {
+                Console.WriteLine("Sayı 0'dır.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VizeFinalOrt/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace VizeFinalOrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vize Notu Giriniz:");
+            int vze = int.Parse(Console.ReadLine());
+            Console.WriteLine("Final Notu Giriniz:");
+            int fnal = int.Parse(Console.ReadLine());
+            double sonuc;
+            sonuc = (vze * 0.4) + (fnal * 0.6);
+
+            if(sonuc<=100 && sonuc>=80)
+            {
+                Console.WriteLine("AA");
+            }
+            else if (sonuc <= 79 && sonuc >= 70)
+            {
+                Console.WriteLine("BA");
+            }
+            else if (sonuc <= 69 && sonuc >= 60)
+            {
+                Console.WriteLine("CC");
+            }
+            else if (sonuc <= 59 && sonuc >= 45)
+            {
+                Console.WriteLine("DD");
+            }
+            else
+            {
+                Console.WriteLine("FF");
+            }
+            Console.ReadKey();
+        }
+    }
+}
